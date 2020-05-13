f740a22ecd75bf7ac6fe88f9c0d6edc742d61256
Florent Guilbaud <florent.guilbaud@gmail.com>
2020-05-13 14:33:22 +0200

    start staging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   end
   config.action_mailer.delivery_method     = :postmark
   config.action_mailer.postmark_settings   = { api_token: ENV['POSTMARK_API_TOKEN'] }
-  config.action_mailer.default_url_options = { host: "bicicouriers.cleverapps.io" }
+  config.action_mailer.default_url_options = { host: "bicicouriers.fr" }
   # Settings specified here will take precedence over those in config/application.rb.
 
 
--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   address: "mail.gandi.net",
   port: 587,
-  domain: 'cleverapps.io',
+  domain: 'bicicouriers.fr',
   user_name: ENV['GMAIL_ADDRESS'],
   password: ENV['GMAIL_APP_PASSWORD'],
   authentication: :login,
