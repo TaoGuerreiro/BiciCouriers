f91b3d3c3198cae22be07f90f408a06a46b599b0
Florent Guilbaud <florent.guilbaud@gmail.com>
2020-11-23 07:03:37 +0100

    cleaning urgence and distance

--- app/javascript/courses/main.js
+++ app/javascript/courses/main.js
@@ -1,7 +1,4 @@
 // import { sweetalert_display } from '../toggle/sweetalert.js';
-import { init_urgences } from '../toggle/init_urgences.js';
-import { getUrgence } from '../toggle/get_urgence.js';
-import { getDistance, getDistTicket } from '../toggle/get_distance.js';
 import { getVolume } from '../toggle/get_volume.js';
 import  { initMap, displayRoute } from '../courses/init_map.js';
 
@@ -14,39 +11,20 @@ const newCourse = () => {
     }
     const puAddress = document.getElementById('course_pickups_attributes_0_address');
     const drAddress = document.getElementById('course_drops_attributes_0_address');
-    const addressInputs = [puAddress, drAddress]
+  
 
-    const urInputs = document.querySelectorAll('.urgence');
     const voInputs = document.querySelectorAll('.volume');
 
     const sousTotals = document.querySelectorAll('.sous-total');
     const toDisplay = document.getElementById('total-course');
     const priceDisplay = document.getElementById('total-price');
 
-    const diDisplay = document.getElementById('kilometers');
     const voTextDisplay = document.getElementById('vo-text-volume');
-    const urDisplay = document.querySelector('.right-urgence');
-    const tiDisplay = document.querySelector('.right-distance');
-    const heDisplay = document.querySelector('.heure');
     const voDisplay = document.querySelector('.right-volume');
     const voInput = document.getElementById('bike');
 
-    const urgence_0_hour = document.querySelector('.urgence-0-hour');
-    const urgence_1_hour = document.querySelector('.urgence-1-hour');
-    const urgence_2_hour = document.querySelector('.urgence-2-hour');
-    const urgence_0_day = document.querySelector('.urgence-0-day');
-    const urgence_1_day = document.querySelector('.urgence-1-day');
-    const urgence_2_day = document.querySelector('.urgence-2-day');
 
-    const puStart = document.getElementById('course_pickups_attributes_0_start_hour');
-    const puEnd = document.getElementById('course_pickups_attributes_0_end_hour');
-    const drStart = document.getElementById('course_drops_attributes_0_start_hour');
-    const drEnd = document.getElementById('course_drops_attributes_0_end_hour');
-    const stDay = document.getElementById('course_pickups_attributes_0_date');
-    const ndDay = document.getElementById('course_drops_attributes_0_date');
-
-    const dropdown = document.querySelectorAll('.form-group');
-
+  
     const getTotal = (sous_total_array_input) => {
       let total = 0
       sous_total_array_input.forEach((number) => {
@@ -58,16 +36,16 @@ const newCourse = () => {
 
     initMap(puAddress, drAddress);
 
-    init_urgences(urgence_0_hour, urgence_0_day, urgence_1_hour, urgence_1_day, urgence_2_hour, urgence_2_day, urInputs)
+    // init_urgences(urgence_0_hour, urgence_0_day, urgence_1_hour, urgence_1_day, urgence_2_hour, urgence_2_day, urInputs)
     // sweetalert_display(addressInputs, urInputs, voInputs);
-    const removeActive = (array) => {
-      // console.log(array)
-      array.forEach((button) => {
-        // console.log(button.classList)
-      button.classList.remove('active');
-      // button.nextElementSibling.classList.remove('font-active');
-     });
-    }
+    // const removeActive = (array) => {
+    // console.log(array)
+    // array.forEach((button) => {
+    // console.log(button.classList)
+    // button.classList.remove('active');
+    // button.nextElementSibling.classList.remove('font-active');
+    //  });
+    // }
 
 // DISTANCE_____________________________________________________________________
     // dropdown.forEach((list) => {
