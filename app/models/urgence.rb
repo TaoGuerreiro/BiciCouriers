808395c84a1b9f6c1aa3b7ead2af6d9dd577eaed
Florent Guilbaud <florent.guilbaud@gmail.com>
2020-11-25 08:51:40 +0100

    adding migration

---
+++ app/models/urgence.rb
@@ -0,0 +1,2 @@
+class Urgence < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_02_034127) do
+ActiveRecord::Schema.define(version: 2020_11_25_074756) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -176,6 +176,13 @@ ActiveRecord::Schema.define(version: 2020_10_02_034127) do
     t.bigint "user_id"
     t.string "state", default: "pending"
     t.index ["user_id"], name: "index_shopping_carts_on_user_id"
+  end
+
+  create_table "urgences", force: :cascade do |t|
+    t.integer "range"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
