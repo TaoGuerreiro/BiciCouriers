1c29e369840f74bbdbe13f213f757478b50102d7
Florent Guilbaud <florent.guilbaud@gmail.com>
2020-12-02 08:14:16 +0100

    Starting price view

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
     purge: [],
     darkMode: false, // or 'media' or 'class'
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          'blue-bici': '#153A7E',
+          'pink-bici': '#FF016C',
+          'yellow-bici': '#FFCC00',
+        },
+      },
     },
     variants: {
       extend: {
