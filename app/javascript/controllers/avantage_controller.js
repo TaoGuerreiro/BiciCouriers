93fb1d4eb048bbcb571d403b6ec48a640c6839d1
Florent Guilbaud <florent.guilbaud@gmail.com>
2021-07-22 00:10:01 +0200

    replacing old stimulus JS notation = ok

--- app/javascript/controllers/avantage_controller.js
+++ app/javascript/controllers/avantage_controller.js
@@ -4,20 +4,19 @@ export default class extends Controller {
   static targets = [ 'card']
 
   connect() {
-    console.log("Connexion de l'avantage controller") 
+    console.log("ADVANTAGE")
   }
 
   fly(event) {
-    console.log(event.target)
-    console.log('test')
-    // cardTarget.style.position = "relative";
-    // card.style.left = (offsetLeft)+"px";
-    // card.style.top = (offsetLeft)+"px";
+      console.log(event.target.classList)
+    // this.cardTargets.forEach((card) => {
+      let offsetLeft = 0;
+      offsetLeft += 1000;
+      event.target.style.position = "relative";
+      event.target.style.left = (offsetLeft)+"px";
+      // this.cardTarget.style.position = "relative";
+      // this.cardTarget.style.left = (offsetLeft)+"px";
+      // this.cardTarget.style.top = (offsetLeft)+"px";
+    // })
   }
 }
-
-
-
-
-
-   
